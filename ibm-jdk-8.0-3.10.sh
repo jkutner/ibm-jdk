28b950b063aa627990a80559c334d71a9a7cdeef
Joe Kutner <jpkutner@gmail.com>
2016-08-26 08:14:07 -0500

    Formatting

--- ibm-jdk-8.0-3.10.sh
+++ ibm-jdk-8.0-3.10.sh
@@ -11,6 +11,7 @@ fi
 docker-compose build
 docker-compose run tar
 
+echo ""
 echo "Build complete!"
 echo "Upload builds/ibm-jdk-${JDK_VERSION}.tar.gz to your server or S3 bucket"
 echo "and run 'heroku config:set JDK_URL_1_8=' with the URL of the file."
