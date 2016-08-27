aab5e35bd67af6b648e5d88d219d21a4ba95788b
Joe Kutner <jpkutner@gmail.com>
2016-08-26 21:49:43 -0500

    Extract from ibmjava image

--- bin/build.sh
+++ bin/build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-sh vendor/ibm-java-sdk-${JDK_VERSION}-x86_64-archive.bin
-
-cd vendor/ibm-java-x86_64-80/
-
-find . -name *.so -exec objcopy --strip-unneeded {} \; #remove debug from .so files
+cd /opt/ibm/java/
 
 ./bin/java -version > version.txt
 echo "${JDK_TAG}" > tag.txt
@@ -13,8 +9,6 @@ TARFILE="jdk.tgz"
 tar czf $TARFILE --exclude='demo' --exclude='src.zip' --exclude='sample' \
     --exclude='docs' --exclude='$TARFILE' --ignore-failed-read *
 
-mv $TARFILE ../../builds/ibm-jdk-${JDK_VERSION}.tar.gz
+mv $TARFILE /app/builds/ibm-jdk-${JDK_VERSION}.tar.gz
 
-cd ../..
-
-rm -rf vendor/ibm-java-x86_64-80
+cd /app
--- ibm-jdk-8.0-3.10.sh
+++ ibm-jdk-8.0-3.10.sh
@@ -2,13 +2,6 @@
 
 export JDK_VERSION="8.0-3.10"
 
-if [ ! -f vendor/ibm-java-sdk-${JDK_VERSION}-x86_64-archive.bin ]; then
-  echo "ERROR: You must download ibm-java-sdk-${JDK_VERSION}-x86_64-archive.bin"
-  echo "       for AMD, Linux, x86_64 platforms and put it in the 'vendor/'"
-  echo "       directory before running this script!"
-  exit 1
-fi
-
 docker-compose -f docker/ibm.yml build
 docker-compose -f docker/ibm.yml run tar
 
